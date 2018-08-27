9f67b798c4a45d36d4beb8240647fb4417d57a6a
Miguel Moreno <hello@miguelmoreno.net>
2018-08-27 04:22:14 -0600

    Tests working and passing

---
+++ index.js
@@ -0,0 +1,10 @@
+//import assert from 'assert'
+//import bip39 from 'bip39'
+//import StellarHDWallet from '../src/stellar-hd-wallet'
+
+const assert = require("assert");
+const bip39 = require("bip39");
+const StellarHDWallet = require("stellar-hd-wallet");
+
+const Mnemonic = "asthma blouse security reform bread mesh roast garage win clock aerobic gauge emotion slender frozen profit duck uphold time perfect giggle drop turn movie";
+const FROM_MNEMONIC_ENGLISH_PUBLIC_KEY_0 = "GBJCYUFJA7VA4GOZV7ZFVB7FGZTLVQTNS7JWJOWQVK6GN7DBUW7L5I5O";
